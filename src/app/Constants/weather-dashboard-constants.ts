a67d4d3d1a172561fbd031cd923e0723706e17e8
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2021-07-02 01:42:28 +0530

    Changed environment file

--- src/app/Constants/weather-dashboard-constants.ts
+++ src/app/Constants/weather-dashboard-constants.ts
@@ -1,6 +1,5 @@
 export const appConfig = {
     remoteHost: 'https://api.openweathermap.org/data/2.5',
-    Key: '0dd5542d536e51ce47540ed11c8f0ebc',
     api: {
         endpoints: {
             weather: '/weather',
