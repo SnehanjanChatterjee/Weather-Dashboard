cf99c632e7ee9588ec84b715720fa501c493921e
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2021-08-28 21:19:10 +0530

    Toast messages working

--- src/app/Models/common.models.ts
+++ src/app/Models/common.models.ts
@@ -2,3 +2,24 @@ export interface Days {
     id: number,
     day: string
 }
+
+export interface ToasterNotification {
+    // toastComponent: any,
+    closeButton: boolean,
+    timeOut: number,
+    extendedTimeOut: number,
+    // disableTimeOut: boolean | 'timeOut' | 'extendedTimeOut',
+    easing: string,
+    easeTime: string | number,
+    // enableHtml: boolean,
+    progressBar: boolean,
+    progressAnimation: 'increasing' | 'decreasing',
+    toastClass: string,
+    positionClass: string,
+    titleClass: string,
+    messageClass: string,
+    tapToDismiss: boolean,
+    onActivateTick: boolean,
+    preventDuplicates: boolean,
+    iconClasses: any
+}
