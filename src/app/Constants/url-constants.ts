a1648c304019be972bdb47623eb9a21fc60d156e
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2021-09-16 01:39:21 +0530

    Added country flag Shifted url constants to new file

---
+++ src/app/Constants/url-constants.ts
@@ -0,0 +1,16 @@
+export const APIUrl = 'https://api.openweathermap.org/data/2.5/';
+export const IconUrl = 'http://openweathermap.org/img/wn/';
+
+export const Api = {
+    endpoints: {
+        weather: '/weather',
+        box: '/box/city',
+        find: '/find',
+        oneCall: '/onecall',
+    }
+}
+
+// https://www.countryflags.io/
+export const FlagURUL = 'https://www.countryflags.io/';
+export const FlagType = "/shiny";
+export const FlagSize = "/64.png";
