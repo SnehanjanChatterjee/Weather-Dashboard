4d60cb3f011113214508a6af3b45847e64b7cf7b
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2021-09-17 01:24:32 +0530

    Humdity Level indicator added

--- src/app/Components/weather-widgets/weather-widgets.component.ts
+++ src/app/Components/weather-widgets/weather-widgets.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { OneAPICallModel } from 'src/app/Models/OneAPICallModel.models';
 import { CurrentWeatherModel } from 'src/app/Models/weather.models';
 
@@ -7,20 +7,42 @@ import { CurrentWeatherModel } from 'src/app/Models/weather.models';
   templateUrl: './weather-widgets.component.html',
   styleUrls: ['./weather-widgets.component.css']
 })
-export class WeatherWidgetsComponent implements OnInit {
+export class WeatherWidgetsComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2) { }
 
   OneCallweather: OneAPICallModel;
   currentWeather: CurrentWeatherModel;
+  gaugeElement: ElementRef;
+  gaugeFillElement: ElementRef;
+  gaugeCoverElement: ElementRef;
+
+  @ViewChild('gauge') set gaugeEl(value: ElementRef) {
+    this.gaugeElement = value;
+  };
+  @ViewChild('gaugeFill') set gaugeFillEl(value: ElementRef) {
+    this.gaugeFillElement = value;
+  };
+  @ViewChild('gaugeCover') set gaugeCoverEl(value: ElementRef) {
+    this.gaugeCoverElement = value;
+    if (this.OneCallweather && this.gaugeCoverElement) {
+      this.setGaugeValue(this.OneCallweather.current.humidity * 0.01);
+    }
+  };
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+  }
+
   @Input() set weatherData(weather: OneAPICallModel) {
     this.OneCallweather = weather;
+    if (this.OneCallweather && this.gaugeCoverElement) {
+      this.setGaugeValue(this.OneCallweather.current.humidity * 0.01);
+    }
   }
-  
+
   @Input() set currentWeatherData(weather: CurrentWeatherModel) {
     this.currentWeather = weather;
   }
@@ -29,4 +51,13 @@ export class WeatherWidgetsComponent implements OnInit {
     return (obj && (Object.keys(obj).length === 0));
   }
 
+  setGaugeValue(value) {
+    if (value < 0 || value > 1) {
+      return;
+    }
+    this.renderer.setStyle(this.gaugeFillElement.nativeElement, 'transform', 'rotate(' + (value / 2) + 'turn)');
+    // this.renderer.createText(this.gaugeCoverElement.nativeElement, value);
+  }
+
 }
+
