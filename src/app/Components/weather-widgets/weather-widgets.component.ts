36d8a8992e44cdf88959996eddb6de348c3946ee
Snehanjan Chatterjee <snehanjan.29@gmail.com>
2022-03-04 00:51:37 +0530

    Added gauge for UVI

--- src/app/Components/weather-widgets/weather-widgets.component.ts
+++ src/app/Components/weather-widgets/weather-widgets.component.ts
@@ -13,12 +13,19 @@ export class WeatherWidgetsComponent implements OnInit, AfterViewInit {
 
   OneCallweather: OneAPICallModel;
   currentWeather: CurrentWeatherModel;
+
   gaugeHumidityElement: ElementRef;
   gaugeHumidityFillElement: ElementRef;
   gaugeHumidityCoverElement: ElementRef;
+
   gaugeCloudElement: ElementRef;
   gaugeCloudFillElement: ElementRef;
   gaugeCloudCoverElement: ElementRef;
+
+  gaugeUVIElement: ElementRef;
+  gaugeUVIFillElement: ElementRef;
+  gaugeUVICoverElement: ElementRef;
+
   widgetNames = {
     humidity: 'Humidity',
     pressure: 'Pressure',
@@ -54,6 +61,19 @@ export class WeatherWidgetsComponent implements OnInit, AfterViewInit {
     }
   };
 
+  @ViewChild('gaugeUVI') set gaugeUVIEl(value: ElementRef) {
+    this.gaugeUVIElement = value;
+  };
+  @ViewChild('gaugeUVIFill') set gaugeUVIFillEl(value: ElementRef) {
+    this.gaugeUVIFillElement = value;
+  };
+  @ViewChild('gaugeUVICover') set gaugeUVICoverEl(value: ElementRef) {
+    this.gaugeUVICoverElement = value;
+    if (this.OneCallweather && this.gaugeUVICoverElement) {
+      this.setGaugeValue(this.OneCallweather.current.uvi / 8, this.gaugeUVICoverElement);
+    }
+  };
+
   ngOnInit(): void {
   }
 
@@ -64,6 +84,7 @@ export class WeatherWidgetsComponent implements OnInit, AfterViewInit {
     this.OneCallweather = weather;
     if (this.OneCallweather && this.gaugeCloudCoverElement && this.gaugeHumidityCoverElement) {
       this.setGaugeValue(this.OneCallweather.current.humidity * 0.01, this.gaugeHumidityFillElement);
+      this.setGaugeValue(this.OneCallweather.current.uvi / 8, this.gaugeUVIFillElement);
       this.setGaugeValue(this.OneCallweather.current.clouds * 0.01, this.gaugeCloudFillElement);
     }
   }
@@ -73,22 +94,36 @@ export class WeatherWidgetsComponent implements OnInit, AfterViewInit {
   }
 
   setGaugeValue(value: number, element: ElementRef) {
-    if (value < 0 || value > 1) {
+    if (value <= 0 || value > 1) {
       return;
     }
     this.renderer.setStyle(element.nativeElement, 'transform', 'rotate(' + (value / 2) + 'turn)');
     // this.renderer.createText(this.gaugeCoverElement.nativeElement, value);
   }
 
-  setGaugeColourBasedOnLevel(value: number) {
-    if (value < 20) {
+  setThresholdColour(value: number) {
+    if (value <= 0) {
+      return '#b4c0be';
+    } else if (value > 0 && value <= 20) {
       return 'red';
-    } else if (value >= 20 && value < 60) {
+    } else if (value > 20 && value <= 60) {
       return 'orange';
-    } else if (value >= 60 && value < 90) {
+    } else if (value > 60 && value <= 90) {
       return '#eded02';
-    } else if (value >= 90) {
+    } else if (value > 90) {
       return 'green';
+    }
+  }
+
+  setUVIThresholdColour(value: number) {
+    if (value <= 0) {
+      return '#b4c0be';
+    } else if (value > 0 && value <= 2) { // You can safely enjoy being outside!
+      return 'green';
+    } else if (value > 2 && value <= 7) { // Seek shade during midday hours! Slip on a shirt, slop on sunscreen and slap on hat!
+      return '#eded02';
+    } else if (value > 7) { // Avoid being outside during midday hours! Make sure you seek shade! Shirt, sunscreen and hat are a must!
+      return 'red';
     }
   }
 
